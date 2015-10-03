7c19389a33a72a2805a714a12e0d4e824af50362
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-03 13:15:25 +0300

    modified structure of project

--- app/src/main/java/com/ubb/app/activities/MainActivity.java
+++ app/src/main/java/com/ubb/app/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ubb.app;
+package com.ubb.app.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ubb.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
