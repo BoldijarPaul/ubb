5485b0840cd6942c6c933edfb92406d56100cc19
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-04 15:50:09 +0300

    loading view from activity

--- app/src/main/java/com/ubb/app/activities/NewsDetailsActivity.java
+++ app/src/main/java/com/ubb/app/activities/NewsDetailsActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.ubb.app.R;
+import com.ubb.app.views.NewsLayout;
 
 public class NewsDetailsActivity extends AppCompatActivity {
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final NewsLayout newsLayout = (NewsLayout) View.inflate(this, R.layout.activity_news_details, null);
+        setContentView(newsLayout);
+    }
 }
