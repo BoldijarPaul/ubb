1474877059e1387700054901477bebb16223e8e8
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-04 12:25:07 +0300

    added async method

--- app/src/main/java/com/ubb/app/service/RSSLoader.java
+++ app/src/main/java/com/ubb/app/service/RSSLoader.java
@@ -1,9 +1,36 @@
 package com.ubb.app.service;
 
+import android.os.AsyncTask;
+
+import com.shirwa.simplistic_rss.RssItem;
+import com.shirwa.simplistic_rss.RssReader;
+
+import java.util.List;
+
 /**
  * Created by Browsing on 10/4/2015.
  */
 public class RSSLoader {
 
-    private static final String RSS_URL="http://www.cs.ubbcluj.ro/feed/";
+    private static final String RSS_URL = "http://www.cs.ubbcluj.ro/feed/";
+
+    public static void getRSSItems(final RSSLoaderCallback callback) {
+        new AsyncTask<Void, Void, List<RssItem>>() {
+            @Override
+            protected List<RssItem> doInBackground(Void... params) {
+                RssReader rssReader = new RssReader(RSS_URL);
+                try {
+                    return rssReader.getItems();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(List<RssItem> rssItems) {
+                callback.onGetRSS(rssItems);
+            }
+        }.execute();
+    }
 }
