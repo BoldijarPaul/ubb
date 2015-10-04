55f48b7cfbedb8c8b3daf9bdbc16874810abbca4
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-04 14:28:25 +0300

    added xml support

--- app/src/main/java/com/ubb/app/xml/XMLHelper.java
+++ app/src/main/java/com/ubb/app/xml/XMLHelper.java
@@ -3,6 +3,9 @@ package com.ubb.app.xml;
 import android.util.Log;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -67,5 +70,24 @@ public class XMLHelper {
         }
         // return DOM
         return doc;
+    }
+
+    public String getValue(Element item, String str) {
+        NodeList n = item.getElementsByTagName(str);
+        return this.getElementValue(n.item(0));
+    }
+
+    public final String getElementValue( Node elem ) {
+        Node child;
+        if( elem != null){
+            if (elem.hasChildNodes()){
+                for( child = elem.getFirstChild(); child != null; child = child.getNextSibling() ){
+                    if( child.getNodeType() == Node.TEXT_NODE  ){
+                        return child.getNodeValue();
+                    }
+                }
+            }
+        }
+        return "";
     }
 }
